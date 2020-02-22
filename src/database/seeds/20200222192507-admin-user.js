e1cf1365ff9767ecf2d8b9524fb4f520915733d8
HermersonDev <ne.hermerson@gmail.com>
2020-02-22 16:27:37 -0300

    :heavy_plus_sign: bcryptjs

---
+++ src/database/seeds/20200222192507-admin-user.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'People',
+      [
+        {
+          name: 'John Doe',
+          isBetaMember: false
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('People', null, {});
+  }
+};
