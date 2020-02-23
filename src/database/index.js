ed879871d029d0a72a666ad74418f17b914d83bd
HermersonDev <ne.hermerson@gmail.com>
2020-02-23 13:26:48 -0300

    :construction: Recipient model

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ import dbConfig from '../config/database';
 
 // MODELS
 import User from '../app/models/User';
+import Recipient from '../app/models/Recipient';
 
-const models = [User];
+const models = [User, Recipient];
 
 class Database {
   constructor() {
