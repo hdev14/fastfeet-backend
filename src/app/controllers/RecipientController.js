635b5a7d03a64b23b067a5e7a8ed43c43014e636
HermersonDev <ne.hermerson@gmail.com>
2020-02-23 15:02:07 -0300

    :construction: fields validation

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,14 +1,49 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .integer()
+        .required(),
+      complement: Yup.string().required(),
+      state: Yup.string()
+        .max(2)
+        .required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
     const recipient = await Recipient.create(req.body);
-    res.json(recipient);
+    return res.json(recipient);
   }
 
   async update(req, res) {
     const { id } = req.params;
     const recipient = await Recipient.findByPk(id);
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number().integer(),
+      complement: Yup.string(),
+      state: Yup.string().max(2),
+      city: Yup.string(),
+      cep: Yup.string()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
     const edited_recipient = await recipient.update(req.body);
     return res.json({ edited_recipient });
   }
