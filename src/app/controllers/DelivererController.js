1bcb4c5203a724a3b52fd1bd9161bbe2e69ab8b8
HermersonDev <ne.hermerson@gmail.com>
2020-02-29 11:09:47 -0300

    :construction: creates crud methods on the DelivererController

--- src/app/controllers/DelivererController.js
+++ src/app/controllers/DelivererController.js
@@ -1,13 +1,41 @@
 import Deliverer from '../models/Deliverer';
 
 class DelivererController {
-  async index(req, res) {}
+  async index(req, res) {
+    const deliverers = await Deliverer.findAll();
+    return res.json(deliverers);
+  }
 
-  async store(req, res) {}
+  async store(req, res) {
+    const { name, email } = req.body;
+    const deliverer = await Deliverer.create({ name, email });
+    return res.json(deliverer);
+  }
 
-  async update(req, res) {}
+  async update(req, res) {
+    const deliverer_id = req.params.id;
+    const deliverer = await Deliverer.findByPk(deliverer_id);
 
-  async delete(req, res) {}
+    if (!deliverer) {
+      return res.status(404).json({ error: 'Deliverer not found' });
+    }
+
+    const { id, name, email } = await deliverer.update(req.body);
+    return res.json({ id, name, email });
+  }
+
+  async delete(req, res) {
+    const deliverer_id = req.params.id;
+    const deliverer = await Deliverer.findByPk(deliverer_id);
+
+    if (!deliverer) {
+      return res.status(404).json({ error: 'Deliverer not found' });
+    }
+
+    await deliverer.destroy();
+
+    return res.send();
+  }
 }
 
 export default new DelivererController();
