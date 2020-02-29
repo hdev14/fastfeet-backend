0720f3136953b4d21ab57a47e71374312c100e54
HermersonDev <ne.hermerson@gmail.com>
2020-02-29 10:34:32 -0300

    :construction: creates deliverer controller

---
+++ src/app/controllers/DelivererController.js
@@ -0,0 +1,13 @@
+import Deliverer from '../models/Deliverer';
+
+class DelivererController {
+  async index(req, res) {}
+
+  async store(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
+
+export default new DelivererController();
