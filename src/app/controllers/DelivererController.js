5a028a1073df3fd4e1e00e414c679524390e78d8
HermersonDev <ne.hermerson@gmail.com>
2020-02-29 11:19:18 -0300

    :construction: creates fields validations

--- src/app/controllers/DelivererController.js
+++ src/app/controllers/DelivererController.js
@@ -1,3 +1,5 @@
+import * as Yup from 'yup';
+
 import Deliverer from '../models/Deliverer';
 
 class DelivererController {
@@ -7,8 +9,20 @@ class DelivererController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { name, email } = req.body;
     const deliverer = await Deliverer.create({ name, email });
+
     return res.json(deliverer);
   }
 
@@ -18,6 +32,15 @@ class DelivererController {
 
     if (!deliverer) {
       return res.status(404).json({ error: 'Deliverer not found' });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
 
     const { id, name, email } = await deliverer.update(req.body);
