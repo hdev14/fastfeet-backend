eba50335b2abd30f331554aae2fe573896cba5fb
HermersonDev <ne.hermerson@gmail.com>
2020-02-29 23:03:05 -0300

    :wrench: nodemailer configs

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: true,
+  auth: {
+    user: 'b958880643ad11',
+    pass: 'b909df1eed3919'
+  },
+  default: {
+    from: 'Equipe FastFeet <noreply@fastfeet.com.br'
+  }
+};
