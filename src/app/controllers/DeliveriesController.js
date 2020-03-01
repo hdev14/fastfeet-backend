9e547abea10d8df7877bc9767e6785b825c657f1
HermersonDev <ne.hermerson@gmail.com>
2020-03-01 18:47:12 -0300

    :construction: creates method for withdrawal and finished orders

--- src/app/controllers/DeliveriesController.js
+++ src/app/controllers/DeliveriesController.js
@@ -1,3 +1,6 @@
+import { Op } from 'sequelize';
+import { isBefore, isAfter, setSeconds, setMinutes, setHours } from 'date-fns';
+
 import Order from '../models/Order';
 import Recipient from '../models/Recipient';
 
@@ -40,7 +43,65 @@ class DeliveriesController {
   }
 
   async update(req, res) {
-    return res.json;
+    const { id, order_id } = req.params;
+    const { op } = req.query;
+
+    const order = await Order.findOne({
+      where: {
+        id: order_id,
+        deliveryman_id: id,
+        canceled_at: null
+      }
+    });
+
+    if (!order) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+
+    const currentDate = new Date();
+
+    if (op === 'start') {
+      const withdrawalStartDate = setSeconds(
+        setMinutes(setHours(currentDate, 8), 0),
+        0
+      );
+      const withdrawalEndDate = setSeconds(
+        setMinutes(setHours(currentDate, 18), 0),
+        0
+      );
+      const isValidDate =
+        isAfter(currentDate, withdrawalStartDate) &&
+        isBefore(currentDate, withdrawalEndDate);
+
+      if (!isValidDate) {
+        return res
+          .status(401)
+          .json({ error: "It's only can withdraw between 08:00 and 18:00" });
+      }
+
+      const numberWithdrawnOrders = await Order.findAll({
+        where: {
+          deliveryman_id: id,
+          start_date: {
+            [Op.ne]: null
+          }
+        }
+      });
+
+      if (numberWithdrawnOrders.length === 5) {
+        return res
+          .status(401)
+          .json({ error: "It's only can withdraw 5 orders for day" });
+      }
+
+      order.start_date = currentDate;
+    } else if (op === 'end') {
+      order.end_date = currentDate;
+    }
+
+    order.save();
+
+    return res.json(order);
   }
 }
 
