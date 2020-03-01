344532d42291c26be5dd0c57cec9301c6366da59
HermersonDev <ne.hermerson@gmail.com>
2020-03-01 15:36:32 -0300

    :construction: creates deliveries controller

---
+++ src/app/controllers/DeliveriesController.js
@@ -0,0 +1,17 @@
+import Order from '../models/Order';
+
+class DeliveriesController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async store(req, res) {
+    return res.json();
+  }
+
+  async update(req, res) {
+    return res.json;
+  }
+}
+
+export default new DeliveriesController();
