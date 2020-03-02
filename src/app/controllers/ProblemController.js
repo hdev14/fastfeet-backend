ea1c97edb8f21ed2d24c85e4a3ebce1a3e9b7688
HermersonDev <ne.hermerson@gmail.com>
2020-03-02 10:08:07 -0300

    :construction: creates method to cancel delivery based on the problem

--- src/app/controllers/ProblemController.js
+++ src/app/controllers/ProblemController.js
@@ -1,7 +1,20 @@
+import DeliveryProblem from '../models/DeliveryProblem';
 import Order from '../models/Order';
 
 class ProblemController {
   async delete(req, res) {
+    const { problem_id } = req.params;
+    const problem = await DeliveryProblem.findByPk(problem_id);
+
+    if (!problem) {
+      return res.status(404).json({ error: 'Problem not found' });
+    }
+
+    const order = await Order.findByPk(problem.delivery_id);
+
+    order.canceled_at = new Date();
+    order.save();
+
     return res.json();
   }
 }
