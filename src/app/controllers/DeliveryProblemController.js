b3764c8121a9d6e5fa44bd7ad28b46ed02c5739b
HermersonDev <ne.hermerson@gmail.com>
2020-03-02 09:28:13 -0300

    :construction: adds delivery_problem controller

---
+++ src/app/controllers/DeliveryProblemController.js
@@ -0,0 +1,13 @@
+import DeliveryProblem from '../models/DeliveryProblem';
+
+class DeliveryProblemController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async store(req, res) {
+    return res.json();
+  }
+}
+
+export default new DeliveryProblemController();
