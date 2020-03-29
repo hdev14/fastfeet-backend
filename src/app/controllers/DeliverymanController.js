b8c3c97bf5a5551dbd5a0ae302d207479c8dd322
HermersonDev <ne.hermerson@gmail.com>
2020-03-28 22:52:18 -0300

    :construction: add filters

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -1,11 +1,20 @@
 import * as Yup from 'yup';
+import { Op } from 'sequelize';
 
 import Deliveryman from '../models/Deliveryman';
 import File from '../models/File';
 
 class DeliverymanController {
   async index(req, res) {
+    const { q } = req.query;
+    const filter = q || '';
+
     const deliverymans = await Deliveryman.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `${filter}%`
+        }
+      },
       attributes: ['id', 'name', 'avatar_id'],
       include: [
         {
